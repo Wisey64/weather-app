6e877dd0a864b56d6cbf522203b39401d87f09ad
Wisey <okun5315455@gmail.com>
2025-10-01 17:51:25 +0200

    Build site for GitHub Pages

--- docs/bundle.js
+++ docs/bundle.js
@@ -409,7 +409,7 @@ eval("{__webpack_require__.r(__webpack_exports__);\n/* harmony export */ __webpa
 /******/ 	
 /******/ 	/* webpack/runtime/publicPath */
 /******/ 	(() => {
-/******/ 		__webpack_require__.p = "/repo-name/";
+/******/ 		__webpack_require__.p = "/weather-app/";
 /******/ 	})();
 /******/ 	
 /******/ 	/* webpack/runtime/nonce */
